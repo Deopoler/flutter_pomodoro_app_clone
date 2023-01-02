8099a201372e829d6a171efd4ef66616bc5f5077
deopoler <50035578+Deopoler@users.noreply.github.com>
2023-01-02 22:20:57 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# pomodoro
+
+Pomodoro App Clone Coding with Nico!
+<https://nomadcoders.co/flutter-for-beginners>
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,162 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static const twentyFiveMinutesInSeconds = 1500;
+  int totalSeconds = twentyFiveMinutesInSeconds;
+  int totalPomodoros = 0;
+  bool isRunning = false;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    if (totalSeconds == 0) {
+      setState(() {
+        totalPomodoros++;
+        totalSeconds = twentyFiveMinutesInSeconds;
+        isRunning = false;
+        timer.cancel();
+      });
+    } else {
+      setState(() {
+        totalSeconds = totalSeconds - 1;
+      });
+    }
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    );
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
+  }
+
+  void onRestartPressed() {
+    setState(() {
+      timer.cancel();
+      totalSeconds = twentyFiveMinutesInSeconds;
+      isRunning = false;
+    });
+  }
+
+  String formatTime(int seconds) {
+    String pad(String str) {
+      return str.padLeft(2, '0');
+    }
+
+    return "${pad((seconds ~/ 60).toString())}:${pad((seconds % 60).toString())}";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                formatTime(totalSeconds),
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 89,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  iconSize: 120,
+                  color: Theme.of(context).cardColor,
+                  onPressed: () {
+                    if (isRunning) {
+                      return onPausePressed;
+                    } else {
+                      return onStartPressed;
+                    }
+                  }(),
+                  icon: Icon(() {
+                    if (isRunning) {
+                      return Icons.pause_circle_outline;
+                    } else {
+                      return Icons.play_circle_outline;
+                    }
+                  }()),
+                ),
+                IconButton(
+                  iconSize: 50,
+                  color: Theme.of(context).cardColor,
+                  onPressed: onRestartPressed,
+                  icon: const Icon(Icons.refresh),
+                ),
+              ],
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40),
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Pomodoros',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context).textTheme.headline1!.color,
+                          ),
+                        ),
+                        Text(
+                          '$totalPomodoros',
+                          style: TextStyle(
+                            fontSize: 58,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context).textTheme.headline1!.color,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
